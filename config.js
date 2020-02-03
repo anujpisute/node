d0b51ec7d8097cf8b26f6573e4a9541fed1ddde4
anujpisute <60571779+anujpisute@users.noreply.github.com>
2020-02-03 10:34:18 +0530

    done

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+	"database": "mongodb+srv://root:root@cluster0-rtsxk.mongodb.net/test?retryWrites=true&w=majority",
+	"port": process.env.PORT || 3000,
+	"secretKey": "YourSecretKey"
+
+}
